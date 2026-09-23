b33172b06fa826dbb1516427a6b3a4cbbb84a227
ShahadatGithub <shahadat.offl@gmail.com>
2026-09-23 13:01:03 +0530

    fixed .toml file and builds

--- frontend/author/src/config/index.js
+++ frontend/author/src/config/index.js
@@ -1,4 +1,4 @@
 export const config = {
- apiBase: import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000/api/v1',
+ apiBase: import.meta.env.VITE_BACKEND_URL,
  appName: 'Lifebookz Author Portal',
 };
