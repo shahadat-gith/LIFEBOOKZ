c55a482c9010c9734e9fce3118b6e587f080c12e
ShahadatGithub <shahadat.offl@gmail.com>
2026-09-23 12:45:24 +0530

    updated index files of worker and index.html to fix favicons issues

--- frontend/developer/worker/index.js
+++ frontend/developer/worker/index.js
@@ -1,6 +1,14 @@
 export default {
   async fetch(request, env) {
-    // env vars are available here, e.g. env.MY_API_KEY
-    return env.ASSETS.fetch(request);
+    const response = await env.ASSETS.fetch(request);
+
+    if (
+      response.status !== 404 ||
+      (request.method !== "GET" && request.method !== "HEAD")
+    ) {
+      return response;
+    }
+
+    return env.ASSETS.fetch(new Request(new URL("/index.html", request.url), request));
   },
 };
